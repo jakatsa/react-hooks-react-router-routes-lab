a9c836626c3ead2045c117ec739982d66fcb30d4
Joe <jakatsa5@gmail.com>
2024-02-14 23:12:02 +0300

    completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayActors = actors.map((actor) => {
+    return (
+      <div key={actor.name}>
+        <h1>Name: {actor.name}</h1>
+        Movies:{" "}
+        <ul key={actor.movies}>
+          <li>{actor.movies[0]}</li>
+          <li>{actor.movies[1]}</li>
+          <li>{actor.movies[2]}</li>
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <div>{displayActors}</div>
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const displayMovies = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h1> title :{movie.title}</h1>
+        <h2>time :{movie.time}</h2>
+        Genres:
+        <ul>
+          <li>{movie.genres[0]}</li>
+          <li>{movie.genres[1]}</li>
+          <li>{movie.genres[2]}</li>
+          {movie.genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div>{displayMovies}</div>
+    </div>
+  );
 }
 
 export default Movies;
